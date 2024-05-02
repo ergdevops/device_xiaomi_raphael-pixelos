b4f6439f7580b21d7621c314afc3d674bcd501b3
ergdevops <honor6firmware@gmail.com>
2024-05-02 21:25:59 +0530

    fwb: revert brightness vibration

--- repo.sh
+++ repo.sh
@@ -2,4 +2,6 @@ repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune 
 cd vendor/aosp
 git revert cba30d055a5dffdf57217c5f59ada565a78edd18
 cd ../..
-
+cd frameworks/base/
+git revert 063e4e2
+cd ../..
