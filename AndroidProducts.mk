c7b8da43facf9df97c7f9cc861a54ce85a3e32e8
ergdevops <honor6firmware@gmail.com>
2024-07-10 14:06:57 +0530

    pixelos'ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_raphael.mk
+    $(LOCAL_DIR)/aosp_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_raphael-user \
-    lineage_raphael-userdebug \
-    lineage_raphael-eng
+    aosp_raphael-ap2a-user \
+    aosp_raphael-ap2a-userdebug \
+    aosp_raphael-ap2a-eng
