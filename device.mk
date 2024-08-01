pwnrazr/device_xiaomi_raphael

raphael: sensors: Support single tap sensor
Just a clone of double_tap sensor
also necessary sepolicy and things

device.mk
@@ -450,7 +450,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     sensors.udfps-custom \
-    sensors.double_tap
+    sensors.double_tap \
+    sensors.single_tap
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \

ergdevops/device_xiaomi_raphael-pixelos

Signed-off-by: firebird11 <hbgassel@gmail.com>

device.mk
@@ -521,7 +521,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     sensors.raphael_udfps \
-    sensors.raphael_double_tap
+    sensors.raphael_double_tap \
+    sensors.raphael_single_tap
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf